366b44565f485b4ef3f0b77d7e2d45a41eec544f
EspenHelgrud <Kompa95@live.no>
2021-01-29 12:24:20 +0100

    cors

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const cors = require('cors');
 
 // App Setup
 const app = express();
 app.use(morgan('combined')) // Logger feil til konsollet
+app.use(cors()); // TODO: bare godkjenn fra klient server
 app.use(bodyParser.json({ type: '*/*' })); // parser alle requests til json
 router(app); // Kaller router med applikasjonen
 
