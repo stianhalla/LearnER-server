853219800dd9eefdd028a440315722267eb82378
Stian <stian.helgerud@outlook.com>
2021-03-16 09:37:50 +0100

    Oppgaver får med synonym ord liste

--- controllers/exercise.js
+++ controllers/exercise.js
@@ -4,7 +4,7 @@
 
 const ErrRes = require('../config/ErrorResponse')
 const SuccRes = require('../config/SuccessResponse')
-const { Exercise, User, Difficulty_level } = require('../models')
+const { Exercise, User, Difficulty_level, Word, Synonym, User_exercise_stat } = require('../models')
 
 
 exports.index = (req, res, next) => {
@@ -13,24 +13,22 @@ exports.index = (req, res, next) => {
         include: [
             {model: User, as: 'author', attributes: ['username']},
             {model: Difficulty_level, as: 'difficulty_level', attributes: ['name', 'points']},
-            {model: User, as: 'users', where: {id: req.user.id}, required: false }
+            {model: User_exercise_stat, as: 'stats', where: {user_id: req.user.id}, attributes: ['completed', 'attempts'], required: false},
+            {
+                model: Word,
+                required: false,
+                as: 'words',
+                attributes: ['word', 'id'],
+                through: {attributes: ['type'], as: 'meta'},
+                include: [{model: Synonym, as: 'synonyms', attributes: ['synonym', 'id']}]
+            }
         ],
         where: { public: true },
-        raw: true,
-        nest: true
     }).then(exercises => {
 
         if(!exercises || exercises.length === 0){return res.status(404).json(new ErrRes('Not Found', ['Cannot find any exercises']));}
 
-        // mapper gjennom alle oppgavene og legger på status om bruker har fullført den
-        const newArr = exercises.map(exercise => {
-            const temp = exercise.users;
-            exercise.users = undefined;
-            exercise.completed = temp.User_exercise_stat.completed === 1
-            return exercise
-        })
-
-        return res.json(new SuccRes('Exercises fetched', newArr))
+        return res.json(new SuccRes('Exercises fetched', exercises))
     }).catch(err => {
         if (!err.errors) {return res.status(500).json(new ErrRes(err.name, [err.message]));}
         return res.status(422).json(new ErrRes(err.name,err.errors.map(error => error.message)));
@@ -40,7 +38,10 @@ exports.index = (req, res, next) => {
 exports.show = (req, res, next) => {
 
     Exercise.findOne({
-        include: [{model: User, as: 'author'}, {model: Difficulty_level, as: 'difficulty_level'}],
+        include: [
+            {model: User, as: 'author'},
+            {model: Difficulty_level, as: 'difficulty_level'},
+        ],
         where: { public: true, id: req.params.id }
     }).then(exercise => {
 
